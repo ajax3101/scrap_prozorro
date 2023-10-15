7e2f994e982f329cf2aef1c7967b2aee8bfbee90
Denys Morozov <ajax3101@gmail.com>
2023-10-15 14:31:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name)
+
+# Функция для получения данных с API ProZorro
+def get_prozorro_data():
+    api_url = "https://prozorro.gov.ua/api/2.5/tenders"
+
+    # Здесь вы можете настроить параметры запроса в соответствии с вашими требованиями
+    params = {
+        "limit": 10  # Пример: получить первые 10 записей
+    }
+
+    response = requests.get(api_url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+@app.route('/')
+def index():
+    prozorro_data = get_prozorro_data()
+    return render_template('index.html', prozorro_data=prozorro_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
