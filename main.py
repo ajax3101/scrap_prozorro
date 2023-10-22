2e5833513dc0900e9e0c2b06958b3e18781de648
Denys Morozov <ajax3101@gmail.com>
2023-10-22 19:15:58 +0300

    add export csv

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from config import CODE_EDRPOU as code_e
 
 
 
@@ -14,7 +15,7 @@ def get_all_pages():
         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
     }
 
-    # r = requests.get(url='https://www.dzo.com.ua/tenders/current?&filter%5Bidentifiers%5D=00022504', headers=headers)
+    # r = requests.get(url='https://www.dzo.com.ua/tenders/current?&filter%5Bidentifiers%5D={code_e}', headers=headers)
     
     # if not os.path.exists("data"):
     #     os.mkdir("data")
@@ -30,8 +31,8 @@ def get_all_pages():
     pages_count = int(soup.find('div', class_="pages relative clear").find_all('a')[-1].text)
     #print(pages_count)
     
-    for i in range(1, 2):
-        url = f"https://www.dzo.com.ua/tenders/current?filter%5Bidentifiers%5D=00022504&page={i}"
+    for i in range(1, pages_count+1):
+        url = f"https://www.dzo.com.ua/tenders/current?filter%5Bidentifiers%5D={code_e}&page={i}"
         #print(url)
         r = requests.get(url=url, headers=headers)
         with open (f"data/page_{i}.html", "w", encoding="utf-8") as file:
@@ -41,22 +42,28 @@ def get_all_pages():
 
 
 def collect_data(pages_count):
-    # cur_date = datetime.now().strftime("%d_%m_%Y")
+    cur_date = datetime.now().strftime("%d_%m_%Y")
 
-    # with open(f"data_{cur_date}.csv", "w") as file:
-    #     writer = csv.writer(file)
+    with open(f"data_{cur_date}.csv", "w") as file:
+        writer = csv.writer(file)
 
-    #     writer.writerow(
-    #         (
-    #             "Tender",
-    #             "URL",
-    #             "CPV"
-    #         )
-    #     )
+        writer.writerow(
+            (
+                "Tender",
+                "URL",
+                "CPV",
+                "Очікувана вартість закупівлі",
+                "Дата оголошення процедури",
+                "Поточний статус процедури",
+                "Ідентифікатор закупівлі",
+                "Організатор закупівлі",
+                "Процедура закупівлі"
+            )
+        )
 
-    # data = []
+    data = []
         
-    for page in range(1, 2):
+    for page in range(1, pages_count):
         with open(f"data/page_{page}.html", encoding="utf-8" ) as file:
             src = file.read()
 
@@ -75,10 +82,38 @@ def collect_data(pages_count):
             cdb = item.find("div", class_="cd newtenderMethod CDB_Number").find_next('span').find_next('span').text
             t_method = item.find("div", class_="cd newtenderMethod").find_next('span').find_next('span').text
 
-            print(f"Tender: {t} - URL: {tl} - INFO: {cpv} - Очікувана вартість закупівлі: {v} - Дата оголошення процедури: {a} - Поточний статус процедури: {s} - Ідентифікатор закупівлі: {t_id} - Організатор закупівлі: {cdb} - Процедура закупівлі: {t_method}")
+            #print(f"Tender: {t} - URL: {tl} - INFO: {cpv} - Очікувана вартість закупівлі: {v} - Дата оголошення процедури: {a} - Поточний статус процедури: {s} - Ідентифікатор закупівлі: {t_id} - Організатор закупівлі: {cdb} - Процедура закупівлі: {t_method}")
+            data.append(
+                {
+                    "t": t,
+                    "tl": tl,
+                    "cpv": cpv,
+                    "v": v,
+                    "a": a,
+                    "s": s,
+                    "t_id": t_id,
+                    "cdb": cdb,
+                    "t_method": t_method
+                }
+            )
+            with open(f"data_{cur_date}.csv", "a") as file:
+                writer = csv.writer(file)
 
+                writer.writerow(
+                    (
+                        t,
+                        tl,
+                        cpv,
+                        v,
+                        a,
+                        s,
+                        t_id,
+                        cdb,
+                        t_method
+                    )
+                )
 
-        print(f"Обработана {page}/{pages_count}")
+        print(f"[INFO] Обработана страница {page}/{pages_count}")
         time.sleep(3)
      
 
